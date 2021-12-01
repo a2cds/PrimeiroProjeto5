f0b32f3dc1329f576e054e13d0096db7f40116f1
Antonio Carlos da Silva <acdasilva@gmail.com>
2021-11-30 21:11:11 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeiroProjeto5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
